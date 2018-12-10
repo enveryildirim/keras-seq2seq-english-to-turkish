91d18adf5ab469f1c30d2baa0415cbd0e7032015
Enver <nwher1994@gmail.com>
2018-12-10 11:26:19 +0300

    initial commit

--- lstm_seq2seq.py
+++ lstm_seq2seq.py
@@ -1,53 +1,3 @@
-'''Sequence to sequence example in Keras (character-level).
-
-This script demonstrates how to implement a basic character-level
-sequence-to-sequence model. We apply it to translating
-short English sentences into short French sentences,
-character-by-character. Note that it is fairly unusual to
-do character-level machine translation, as word-level
-models are more common in this domain.
-
-# Summary of the algorithm
-
-- We start with input sequences from a domain (e.g. English sentences)
-    and correspding target sequences from another domain
-    (e.g. French sentences).
-- An encoder LSTM turns input sequences to 2 state vectors
-    (we keep the last LSTM state and discard the outputs).
-- A decoder LSTM is trained to turn the target sequences into
-    the same sequence but offset by one timestep in the future,
-    a training process called "teacher forcing" in this context.
-    Is uses as initial state the state vectors from the encoder.
-    Effectively, the decoder learns to generate `targets[t+1...]`
-    given `targets[...t]`, conditioned on the input sequence.
-- In inference mode, when we want to decode unknown input sequences, we:
-    - Encode the input sequence into state vectors
-    - Start with a target sequence of size 1
-        (just the start-of-sequence character)
-    - Feed the state vectors and 1-char target sequence
-        to the decoder to produce predictions for the next character
-    - Sample the next character using these predictions
-        (we simply use argmax).
-    - Append the sampled character to the target sequence
-    - Repeat until we generate the end-of-sequence character or we
-        hit the character limit.
-
-# Data download
-
-English to French sentence pairs.
-http://www.manythings.org/anki/fra-eng.zip
-
-Lots of neat sentence pairs datasets can be found at:
-http://www.manythings.org/anki/
-
-# References
-
-- Sequence to Sequence Learning with Neural Networks
-    https://arxiv.org/abs/1409.3215
-- Learning Phrase Representations using
-    RNN Encoder-Decoder for Statistical Machine Translation
-    https://arxiv.org/abs/1406.1078
-'''
 from __future__ import absolute_import, division, print_function
 
 from keras.models import Model
@@ -56,14 +6,13 @@ import numpy as np
 import tensorflow as tf
 
 
-batch_size = 64  # Batch size for training.
-epochs = 100  # Number of epochs to train for.
-latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 10000  # Number of samples to train on.
-# Path to the data txt file on disk.
+batch_size = 64 
+epochs = 50  
+latent_dim = 256  
+num_samples = 10000  
 data_path = 'tur.txt'
 
-# Vectorize the data.
+# Verinin vektörize edilmesi.
 input_texts = []
 target_texts = []
 input_characters = set()
@@ -72,8 +21,8 @@ with open(data_path, 'r', encoding='utf-8') as f:
     lines = f.read().split('\n')
 for line in lines[: min(num_samples, len(lines) - 1)]:
     input_text, target_text = line.split('\t')
-    # We use "tab" as the "start sequence" character
-    # for the targets, and "\n" as "end sequence" character.
+    # Başlangıç cümlesi için tab kullanıldı
+    # "\n" as bitiş cümlesi karakteri.
     target_text = '\t' + target_text + '\n'
     input_texts.append(input_text)
     target_texts.append(target_text)
@@ -116,57 +65,44 @@ for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
     for t, char in enumerate(input_text):
         encoder_input_data[i, t, input_token_index[char]] = 1.
     for t, char in enumerate(target_text):
-        # decoder_target_data is ahead of decoder_input_data by one timestep
+        
         decoder_input_data[i, t, target_token_index[char]] = 1.
         if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
             decoder_target_data[i, t - 1, target_token_index[char]] = 1.
 
-# Define an input sequence and process it.
+# Bir giriş cümlesi tanımlayalım ve işleyelim.
 encoder_inputs = Input(shape=(None, num_encoder_tokens))
 encoder = LSTM(latent_dim, return_state=True)
 encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-# We discard `encoder_outputs` and only keep the states.
+#`encoder_outputs` atıyoruz ve sadece durumları tutuyoruz.
 encoder_states = [state_h, state_c]
 
-# Set up the decoder, using `encoder_states` as initial state.
+# decoder ayarlıyoruz tuttuğumuz `encoder_states` ile başlangıç durumu olarak.
 decoder_inputs = Input(shape=(None, num_decoder_tokens))
-# We set up our decoder to return full output sequences,
-# and to return internal states as well. We don't use the
-# return states in the training model, but we will use them in inference.
+# Tam çıkış dizilerini döndürmek için decoderi kurduk
+# ve iç durumları da iade etmek için. 
+# Eğitim modelinde geri dönüş durumlarını kullanmıyoruz, ancak bunları çıkarımda kullanacağız.
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                      initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
-# Define the model that will turn
-# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+# model tanımlıyoruz
+# Model bize döndürecek `encoder_input_data` & `decoder_input_data` içinde `decoder_target_data`
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
-# Run training
+
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 for i in range(10):
     model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
           batch_size=batch_size,
           epochs=epochs,
           validation_split=0.2)
-    # Save model
+   
     model.save('s2s_tur'+str(i)+'.h5')
 
-# Next: inference mode (sampling).
-# Here's the drill:
-# 1) encode input and retrieve initial decoder state
-# 2) run one step of decoder with this initial state
-# and a "start of sequence" token as target.
-# Output will be the next target token
-# 3) Repeat with the current target token and current states
 
-# Define sampling models
-
-#print(encoder_inputs)
-#print(encoder_states)
 
 encoder_model = Model(encoder_inputs, encoder_states)
 
@@ -181,8 +117,6 @@ decoder_model = Model(
     [decoder_inputs] + decoder_states_inputs,
     [decoder_outputs] + decoder_states)
 
-# Reverse-lookup token index to decode sequences back to
-# something readable.
 reverse_input_char_index = dict(
     (i, char) for char, i in input_token_index.items())
 reverse_target_char_index = dict(
@@ -190,48 +124,43 @@ reverse_target_char_index = dict(
 
 
 def decode_sequence(input_seq):
-    # Encode the input as state vectors.
+    # Durum vektörü olarak giriş kodlanması.
     states_value = encoder_model.predict(input_seq)
 
-    # Generate empty target sequence of length 1.
+   
     target_seq = np.zeros((1, 1, num_decoder_tokens))
-    # Populate the first character of target sequence with the start character.
     target_seq[0, 0, target_token_index['\t']] = 1.
 
-    # Sampling loop for a batch of sequences
-    # (to simplify, here we assume a batch of size 1).
+  
     stop_condition = False
     decoded_sentence = ''
     while not stop_condition:
         output_tokens, h, c = decoder_model.predict(
             [target_seq] + states_value)
 
-        # Sample a token
+     
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
         sampled_char = reverse_target_char_index[sampled_token_index]
         decoded_sentence += sampled_char
 
-        # Exit condition: either hit max length
-        # or find stop character.
+        
         if (sampled_char == '\n' or
            len(decoded_sentence) > max_decoder_seq_length):
             stop_condition = True
 
-        # Update the target sequence (of length 1).
+    
         target_seq = np.zeros((1, 1, num_decoder_tokens))
         target_seq[0, 0, sampled_token_index] = 1.
 
-        # Update states
+        
         states_value = [h, c]
 
     return decoded_sentence
 
 
 for seq_index in range(100):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
     input_seq = encoder_input_data[seq_index: seq_index + 1]
     decoded_sentence = decode_sequence(input_seq)
     print('-')
-    print('Input sentence:', input_texts[seq_index])
-    print('Decoded sentence:', decoded_sentence)
+    print('Giriş Cümlesi', input_texts[seq_index])
+    print('Çıkış Cümlesi', decoded_sentence)
--- lstm_seq2seq_restore.py
+++ lstm_seq2seq_restore.py
@@ -1,30 +1,17 @@
-'''Restore a character-level sequence to sequence model from disk and use it
-to generate predictions.
-
-This script loads the s2s.h5 model saved by lstm_seq2seq.py and generates
-sequences from it.  It assumes that no changes have been made (for example:
-latent_dim is unchanged, and the input data and model architecture are unchanged).
-
-See lstm_seq2seq.py for more details on the model architecture and how
-it is trained.
-'''
 from __future__ import print_function
 
 from keras.models import Model, load_model
 from keras.layers import Input
 import numpy as np
 
-batch_size = 64  # Batch size for training.
-epochs = 10  # Number of epochs to train for.
-latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 10000  # Number of samples to train on.
-# Path to the data txt file on disk.
-data_path = 'fra.txt'
+batch_size = 64 
+epochs = 10  
+latent_dim = 256  
+num_samples = 10000 
 
-# Vectorize the data.  We use the same approach as the training script.
-# NOTE: the data must be identical, in order for the character -> integer
-# mappings to be consistent.
-# We omit encoding target_texts since they are not needed.
+data_path = 'tur.txt'
+
+
 input_texts = []
 target_texts = []
 input_characters = set()
@@ -33,8 +20,7 @@ with open(data_path, 'r', encoding='utf-8') as f:
     lines = f.read().split('\n')
 for line in lines[: min(num_samples, len(lines) - 1)]:
     input_text, target_text = line.split('\t')
-    # We use "tab" as the "start sequence" character
-    # for the targets, and "\n" as "end sequence" character.
+    
     target_text = '\t' + target_text + '\n'
     input_texts.append(input_text)
     target_texts.append(target_text)
@@ -52,11 +38,7 @@ num_decoder_tokens = len(target_characters)
 max_encoder_seq_length = max([len(txt) for txt in input_texts])
 max_decoder_seq_length = max([len(txt) for txt in target_texts])
 
-print('Number of samples:', len(input_texts))
-print('Number of unique input tokens:', num_encoder_tokens)
-print('Number of unique output tokens:', num_decoder_tokens)
-print('Max sequence length for inputs:', max_encoder_seq_length)
-print('Max sequence length for outputs:', max_decoder_seq_length)
+h)
 
 input_token_index = dict(
     [(char, i) for i, char in enumerate(input_characters)])
@@ -71,8 +53,8 @@ for i, input_text in enumerate(input_texts):
     for t, char in enumerate(input_text):
         encoder_input_data[i, t, input_token_index[char]] = 1.
 
-# Restore the model and construct the encoder and decoder.
-model = load_model('s2s_fra.h5')
+
+model = load_model('s2s_tur9.h5')
 
 encoder_inputs = model.input[0]   # input_1
 encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output   # lstm_1
@@ -93,58 +75,54 @@ decoder_model = Model(
     [decoder_inputs] + decoder_states_inputs,
     [decoder_outputs] + decoder_states)
 
-# Reverse-lookup token index to decode sequences back to
-# something readable.
+
 reverse_input_char_index = dict(
     (i, char) for char, i in input_token_index.items())
 reverse_target_char_index = dict(
     (i, char) for char, i in target_token_index.items())
 
 
-# Decodes an input sequence.  Future work should support beam search.
+
 def decode_sequence(input_seq):
-    # Encode the input as state vectors.
+    
     states_value = encoder_model.predict(input_seq)
 
-    # Generate empty target sequence of length 1.
+    
     target_seq = np.zeros((1, 1, num_decoder_tokens))
-    # Populate the first character of target sequence with the start character.
+    
     target_seq[0, 0, target_token_index['\t']] = 1.
 
-    # Sampling loop for a batch of sequences
-    # (to simplify, here we assume a batch of size 1).
+    
     stop_condition = False
     decoded_sentence = ''
     while not stop_condition:
         output_tokens, h, c = decoder_model.predict(
             [target_seq] + states_value)
 
-        # Sample a token
+       
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
         sampled_char = reverse_target_char_index[sampled_token_index]
         decoded_sentence += sampled_char
 
-        # Exit condition: either hit max length
-        # or find stop character.
+        
         if (sampled_char == '\n' or
            len(decoded_sentence) > max_decoder_seq_length):
             stop_condition = True
 
-        # Update the target sequence (of length 1).
+        
         target_seq = np.zeros((1, 1, num_decoder_tokens))
         target_seq[0, 0, sampled_token_index] = 1.
 
-        # Update states
+        
         states_value = [h, c]
 
     return decoded_sentence
 
 
 for seq_index in range(100):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
+    
     input_seq = encoder_input_data[seq_index: seq_index + 1]
     decoded_sentence = decode_sequence(input_seq)
     print('-')
-    print('Input sentence:', input_texts[seq_index])
-    print('Decoded sentence:', decoded_sentence)
+    print('Giriş Cümlesi:', input_texts[seq_index])
+    print('Çıkış Cümlesi:', decoded_sentence)
